bdc2e59ab3264d405f2b9574169e90d394cb47d3
Ivan Rafi Susanto <susantoivan0611@gmail.com>
2021-09-14 13:42:25 +0700

    Add CRUD Controller

---
+++ controller/crud.js
@@ -0,0 +1,42 @@
+const crudModel = require("../model/CRUDModel");
+const platform = require("../platform");
+
+const uniqid = require("uniqid");
+
+exports.insertMenu = async (req, res) => {
+  let photo;
+  if (req.files) {
+    var file = req.files.photo;
+    var extension = req.files.photo.name.split(".");
+    extension = extension[extension.length - 1];
+    photo = `${uniqid()}.${extension}`;
+
+    file.mv(platform.projectDir + "/images/" + photo, function (err) {
+      if (err) console.log(err);
+    });
+  }
+
+  var data = {
+    menuName: req.body.menuName,
+    menuDescription: req.body.menuDescription,
+    quantity: req.body.quantity,
+    price: req.body.price,
+    photo,
+  };
+
+  let result = crudModel.insertMenu(data);
+  result
+    .then(function (result) {
+      res.json({
+        status: 200,
+        success: true,
+      });
+    })
+    .catch(function (err) {
+      res.json({
+        status: 500,
+        success: false,
+        message: err,
+      });
+    });
+};
